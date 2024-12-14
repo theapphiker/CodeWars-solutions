5ef1516ab93d1e8fcecaa024b1f289bd8773cc31
theapphiker <84999858+theapphiker@users.noreply.github.com>
2024-12-14 09:15:54 -0500

    Update user_classification.sql

--- PostgreSQL/user_classification.sql
+++ PostgreSQL/user_classification.sql
@@ -1,4 +1,4 @@
--- This PostgresSQL accurately counts the number of users in each class, with a special rule: If a user belongs to both classes, they should only be counted under class 'b'.
+-- This PostgreSQL accurately counts the number of users in each class, with a special rule: If a user belongs to both classes, they should only be counted under class 'b'.
 -- Link to challenge: https://www.codewars.com/kata/64a579dcdddc250831dfa2b7
 
 SELECT class,COUNT(*)
